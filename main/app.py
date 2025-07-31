352c5914d48272752957ccbb4ea1db4ac748782b
Om Amar <emailomamar@gmail.com>
2025-07-31 16:00:18 +0530

    updated

--- main/app.py
+++ main/app.py
@@ -2,48 +2,42 @@ from flask import Flask,request,jsonify
 from flask_cors import CORS
 import os,json
 from response import response
-# from gettable import gettable
+from gettable import gettable
+from geturl import geturl
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 app = Flask(__name__,template_folder='templates')
 CORS(app)
 
-# DATA_PREPARER = {
-#     "type": "function",
-#     "function": {
-#         "name": "extract_data",
-#         "description": (
-#             "Write complete code to load the dataset from the given URL or source, "
-#             "without modifying, renaming, or assuming any column names. "
-#             "Only include essential imports (like pandas) and the code to load the dataset. "
-#             "Ensure the code runs correctly and stores the data in a variable."
-#         ),
-#         "parameters": {
-#             "type": "object",
-#             "properties": {
-#                 "code": {
-#                     "type": "string",
-#                     "description": (
-#                         "The complete code to load the dataset. Must include necessary imports, "
-#                         "and store the loaded data in a variable (like df)."
-#                     )
-#                 },
-#                 "data_type": {
-#                     "type": "string",
-#                     "description": "The data type of the variable storing the dataset (e.g., pandas.DataFrame)"
-#                 }
-#             },
-#             "required": ["code", "data_type"],
-#             "additionalProperties": False
-#         },
-#         "strict": True
-#     }
-# }
+DATA_PREPARER = {
+    "type": "function",
+    "function": {
+        "name": "decide_type",
+        "description": (
+            "Choose one of the following which would work for getting data mentioned in the problem statement"
+        ),
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "index": {
+                    "type": "integer",
+                    "description": (
+                        "Choose one of the following which would work for getting data mentioned in the problem statement"
+                    )
+                }
+            },
+            "required": ["index"],
+            "additionalProperties": False
+        },
+        "strict": True
+    }
+}
 
 Dataset = [
-    "1. Data Scraping and Extracting Single Table using Pandas", # gettable.py
-    "2. Data Scraping from some website some Other Way",
-    "3. Neither 1 nor 2" 
+    "1. One website has the relevant table/list", # gettable.py
+    "2. Data Scraping some other way",
+    "3. Getting Data from somewhere else",
+    "4. Does not mention a loadable dataset/list"
 ]
 
 @app.route('/analyze',methods=['POST'])
@@ -53,24 +47,46 @@ def analyze():
     messages = [
         {
             "role":"user",
-            "content":file
+            "content":file+'\n'+'\n'.join(Dataset)+'\n\nWhich is the correct way to proceed out of these four'
         }
     ]
 
-    # else:
-    #     for i in range(10):
-    #         d = response(DATA_PREPARER,messages)
-    #         d = json.loads(d["choices"][0]["message"]["tool_calls"][0]["function"]["arguments"])
-    #         code,type1 = d["code"],d["data_type"]
-    #         messages.append({"role":"agent","content":(d["code"],d["data_type"])})
+    clean = [
+        {
+            "role":"user",
+            "content":file
+        }
+    ]
+    print(messages)
+    f = response(DATA_PREPARER,messages)
+    data = json.loads(f["choices"][0]["message"]["tool_calls"][0]["function"]["arguments"])
+    Lindex = data['index']
 
-    #         flag,output = isok(code,type1)
-    #         finaloutput = output
-    #         if flag:
-    #             break
-            
+    flag = False
+    df = None
 
-    #         print(code,type1)
+    if Lindex==1:
+        # print("Data Scraping and Extracting Single Table using Pandas")
+        try:
+            df = gettable(geturl(file),clean)
+        except:
+            pass
+    elif Lindex==2:
+        print("Data Scraping from some website some other way")
+    elif Lindex==3:
+        print("Getting Data from somewhere else")
+    else:
+        flag = True
+    
+    if flag or df==None:
+        print("Does not mention a loadable dataset")
+
+        # ise idhar hi khatam krdo 
+
+        L = [1,'titanic',0.485782,'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGNgYAAAAAMAASsJTYQAAAAASUVORK5CYII=']
+        return jsonify(json.dumps(L))
+
+
 
     L = [1,'titanic',0.485782,'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGNgYAAAAAMAASsJTYQAAAAASUVORK5CYII=']
     return jsonify(json.dumps(L))
